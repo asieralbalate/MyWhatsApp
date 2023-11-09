4ed4790922761b1e19319b8483e94b5697c3f2b6
asieralbalate <alualb1634@ieselcaminas.org>
2023-11-09 01:02:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mywhatsapp/Chats.kt
@@ -0,0 +1,183 @@
+package com.example.mywhatsapp
+
+import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+@Composable
+fun Chats() {
+    VistaContacto()
+}
+
+data class Contacto(
+    var grupo: String,
+    var nombre : String,
+    @DrawableRes var photo: Int
+)
+
+fun getContacto(): List<Contacto> {
+    return listOf(
+        Contacto(
+            "Departamento Informática",
+            "Jefe de Departamento",
+            R.drawable.photo
+        ),
+        Contacto(
+            "Departamento Informática",
+            "Tutora DAM",
+            R.drawable.photo1
+        ),
+        Contacto(
+            "Departamento Informática",
+            "Tutor DAW",
+            R.drawable.photo2
+        ),
+        Contacto(
+            "Departamento Informática",
+            "Tutora ASIX",
+            R.drawable.photo3
+        ),
+        Contacto(
+            "Comunidad Propietarios",
+            "Presidenta",
+            R.drawable.photo4
+        ),
+        Contacto(
+            "Comunidad Propietarios",
+            "Secretaria",
+            R.drawable.photo5
+        ),
+        Contacto(
+            "Comunidad Propietarios",
+            "Administrador",
+            R.drawable.photo6
+        ),
+        Contacto(
+            "Gimnasio",
+            "Entrenadora",
+            R.drawable.photo7
+        ),
+        Contacto(
+            "Gimnasio",
+            "Nutricionista",
+            R.drawable.photo8
+        ),
+        Contacto(
+            "Gimnasio",
+            "Fisioterapeuta",
+            R.drawable.photo9
+        )
+    )
+}
+
+@Composable
+fun MyDropDownMenu(showMenu: Boolean){
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@SuppressLint("ResourceType")
+@Composable
+fun VistaContacto(){
+    val contactos : Map<String, List<Contacto>> = getContacto().groupBy { it.grupo }
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        contactos.forEach { (items, miContacto) ->
+            stickyHeader {
+                Text(
+                    text = items,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.LightGray),
+                    fontSize = 16.sp
+                )
+            }
+            items(miContacto) { contacto ->
+                LineaContacto(contacto = contacto)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun LineaContacto(contacto: Contacto){
+    val scope = rememberCoroutineScope()
+    var showMenu by remember { mutableStateOf(false) }
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.pointerInput(true) {
+            detectTapGestures(
+                onLongPress = {
+                    scope.launch {
+                        showMenu = !showMenu
+                    }
+                }
+            )
+        }){
+        DropdownMenu(
+            expanded = showMenu,
+            onDismissRequest = { showMenu = false },
+            Modifier.width(150.dp)
+        ) {
+            DropdownMenuItem(
+                text = { Text(text = "Salir del grupo", color = Color.Black) },
+                onClick = { /*TODO*/ })
+            DropdownMenuItem(
+                text = { Text(text = "Info. del grupo", color = Color.Black) },
+                onClick = { /*TODO*/ })
+            DropdownMenuItem(
+                text = { Text(text = "Crear acceso directo", color = Color.Black) },
+                onClick = { /*TODO*/ })
+        }
+        Image(
+            painter = painterResource(id = contacto.photo),
+            contentDescription = "Avatar",
+            modifier = Modifier
+                .padding(start = 10.dp, top = 5.dp, bottom = 5.dp)
+                .size(75.dp)
+                .clip(shape = RoundedCornerShape(40.dp)),
+            contentScale = ContentScale.Crop,
+        )
+        Text(text = contacto.nombre,
+            fontSize = 17.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            modifier = Modifier.padding(start = 10.dp))
+    }
+}
---
+++ app/src/main/java/com/example/mywhatsapp/Portada.kt
@@ -0,0 +1,144 @@
+package com.example.mywhatsapp
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.mywhatsapp.ui.theme.VerdeNormal
+import com.example.mywhatsapp.ui.theme.VerdeOscuro
+import kotlinx.coroutines.launch
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Portada() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        rememberTopAppBarState()
+    )
+    Scaffold(
+        topBar = { MyTopBar(scrollBehavior) },
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        floatingActionButton = {
+            MyFAB()
+        },
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = it.calculateTopPadding())
+            ) {
+                MyTabs()
+            }
+
+        }
+    )
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(scrollBehavior: TopAppBarScrollBehavior) {
+    var showMenu by remember { mutableStateOf(false) }
+    TopAppBar(
+        title = { Text(text = "MyWhatsApp", color = Color.White) },
+        colors = TopAppBarDefaults.largeTopAppBarColors(containerColor = VerdeNormal),
+        actions = {
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(
+                    imageVector = Icons.Filled.Share,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
+
+@Composable
+fun MyFAB() {
+    FloatingActionButton(
+        onClick = { /*TODO*/ },
+        containerColor = VerdeOscuro,
+        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+    ) {
+        Icon(imageVector = Icons.Filled.Check, contentDescription = "Check", tint = Color.White)
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MyTabs() {
+    val pagerState = rememberPagerState(initialPage = 0)
+    val scope = rememberCoroutineScope()
+    val titles = listOf("Chats", "Novedades", "Llamadas")
+    Column {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            containerColor = VerdeNormal,
+            contentColor = Color.White
+        ) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = { scope.launch { pagerState.animateScrollToPage(page = index) } },
+                    text = { Text(text = title) }
+                )
+            }
+        }
+        HorizontalPager(pageCount = 3, state = pagerState) { page ->
+            when (page) {
+                0 -> Chats()
+                1 -> Novedades()
+                2 -> Llamadas()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
