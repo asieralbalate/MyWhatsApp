fef854a34a1aa2cf86d213323c064e46ca5962b8
asieralbalate <alualb1634@ieselcaminas.org>
2023-11-13 00:16:23 +0100

    uff

--- app/src/main/java/com/example/mywhatsapp/Novedades.kt
+++ app/src/main/java/com/example/mywhatsapp/Novedades.kt
@@ -1,14 +1,48 @@
 package com.example.mywhatsapp
 
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.example.mywhatsapp.ui.theme.VerdeOscuro
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
 fun Novedades() {
-    Box(modifier = Modifier.fillMaxSize()){
-
+    Column(modifier = Modifier.fillMaxSize()) {
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            containerColor = VerdeOscuro,
+            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+            modifier = Modifier.size(60.dp)
+        ) {
+            val image =
+                AnimatedImageVector.animatedVectorResource(R.drawable.icono_trans)
+            var atEnd by remember { mutableStateOf(false) }
+            Image(
+                painter = rememberAnimatedVectorPainter(image, atEnd),
+                contentDescription = "VectorDrawable",
+                modifier = Modifier.size(32.dp).clickable {
+                    atEnd = !atEnd
+                },
+                contentScale = ContentScale.Crop,
+            )
+        }
     }
 }
--- app/src/main/java/com/example/mywhatsapp/Portada.kt
+++ app/src/main/java/com/example/mywhatsapp/Portada.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
