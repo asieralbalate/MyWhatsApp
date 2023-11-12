9a7171031cae34cc8cbaabbf665f7edeb8eb7527
asieralbalate <alualb1634@ieselcaminas.org>
2023-11-12 23:12:13 +0100

    uff

--- app/src/main/java/com/example/mywhatsapp/Novedades.kt
+++ app/src/main/java/com/example/mywhatsapp/Novedades.kt
@@ -1,9 +1,14 @@
 package com.example.mywhatsapp
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun Novedades() {
-    Text(text = "Novedades")
+    Box(modifier = Modifier.fillMaxSize()){
+
+    }
 }
