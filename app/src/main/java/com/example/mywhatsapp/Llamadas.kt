bb9d35ff50fb973a8901c0b4a1cf84fa2f5111a8
asieralbalate <alualb1634@ieselcaminas.org>
2023-11-15 00:36:07 +0100

    final

--- app/src/main/java/com/example/mywhatsapp/Llamadas.kt
+++ app/src/main/java/com/example/mywhatsapp/Llamadas.kt
@@ -1,9 +1,44 @@
 package com.example.mywhatsapp
 
-import androidx.compose.material3.Text
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun Llamadas() {
-    Text(text = "Llamadas")
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        AnimatedVectorDrawable3()
+    }
+}
+
+@OptIn(ExperimentalAnimationGraphicsApi::class)
+@Composable
+fun AnimatedVectorDrawable3(){
+    val image =
+        AnimatedImageVector.animatedVectorResource(R.drawable.icono_cara
+        )
+    var atEnd by remember { mutableStateOf(false) }
+    Image(
+        painter = rememberAnimatedVectorPainter(image, atEnd),
+        contentDescription = "VectorDrawable",
+        modifier = Modifier
+            .clickable {
+                atEnd = !atEnd
+            },
+        contentScale = ContentScale.Crop,
+    )
 }
